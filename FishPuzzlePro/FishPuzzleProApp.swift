6e23607fcfda023384ef77098f5ce256718e9469
LucasBKnight <lucasbknight@gmail.com>
2023-03-01 09:49:11 -0800

    Initial Commit

---
+++ FishPuzzlePro/FishPuzzleProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishPuzzleProApp.swift
+//  FishPuzzlePro
+//
+//  Created by Lucas Knight on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct FishPuzzleProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
